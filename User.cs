100bbc9ebe74b8095c9e61da373c8a9362d06c36
Александр Ильин <sasha8@list.ru>
2024-01-02 19:42:58 +0300

    Добавили функционал базы данных

---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserApp_WPF
+{
+    class User
+    {
+        int id { get;set;}
+        string login, pass, email;
+
+        public User() { }
+
+        public User(int id, string login, string pass, string email)
+        {
+            this.id = id;
+            this.login = login;
+            this.pass = pass;
+            this.email = email;
+        }
+    }
+}
