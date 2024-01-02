f33fe91c0501302db32e34f572be8bd3fb20fd2e
Александр Ильин <sasha8@list.ru>
2024-01-02 13:34:00 +0300

    Сдклан обработчик нажатия кнопки

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,47 @@ namespace UserApp_WPF
         {
             InitializeComponent();
         }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string pass_2 = passBox_2.Password.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+
+            if(login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Логин слишком короткий!";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if(pass.Length < 5)
+            {
+                passBox.ToolTip = "Пароль слишком короткий!";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (pass != pass_2)
+            {
+                passBox_2.ToolTip = "Пароли не совпадают!";
+                passBox_2.Background = Brushes.DarkRed;
+            }
+            else if (email.Length < 5 || !email.Contains('@') || !email.Contains('.'))
+            {
+                textBoxEmail.ToolTip = "Emeil неверный!";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+                passBox_2.ToolTip = "";
+                passBox_2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Всё хорошо!");
+            }
+        }
     }
 }
